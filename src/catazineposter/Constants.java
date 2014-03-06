579f162e53986a274d45931e2da0634a350f28c5
ICODER <ahmad.melegy@gmail.com>
2014-03-06 23:26:15 +0200

    first commit

---
+++ src/catazineposter/Constants.java
@@ -0,0 +1,17 @@
+package catazineposter;
+
+/**
+ *
+ * @author Ahmad
+ */
+public class Constants {
+
+    public static final String NEW_ENGLISH = "/resources/new_en.svg";
+    public static final String NEW_ARABIC = "/resources/new_ar.svg";
+    public static final String OLD_ARABIC = "/resources/old_ar.svg";
+    public static final String OLD_ENGLISH = "/resources/old_en.svg";
+    public static final String TEMP_LOCATION = "/resources/temp.svg";
+    public static final String AUTHOR_SYMBOL = "changetoauthor";
+    public static final String TITLE_SYMBOL = "changetotitle";
+
+}
---
+++ src/catazineposter/MainClass.java
@@ -0,0 +1,29 @@
+package catazineposter;
+
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author ahmad
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+        }
+
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        mainFrame.setLocationRelativeTo(null);
+    }
+
+}
